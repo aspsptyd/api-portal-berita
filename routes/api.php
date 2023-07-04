2987cb5bcf48c8c034d216da71af5083d013941b
aspsptyd <devadiroot@gmail.com>
2023-07-04 18:36:50 +0700

    Tambah route get detail post versi 2 membedakan result outputnya disesuaikan dengan request frontend

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,5 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/posts', [PostController::class, 'index']);
+Route::get('/post/{id}', [PostController::class, 'show']);
+Route::get('/post2/{id}', [PostController::class, 'show2']);
