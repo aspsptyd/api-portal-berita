e8ed288e33feb77d605455fca9efc1461e1d3fc4
aspsptyd <devadiroot@gmail.com>
2023-07-04 18:29:32 +0700

    Menambahkan resource untuk detail post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PostDetailResource;
+use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
@@ -10,7 +12,18 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::all();
+        return PostResource::collection($posts);
+    }
 
-        return response()->json($posts); 
+    public function show($id) 
+    {
+        $post = Post::with('writer:id,username,email,firstname,created_at')->findOrFail($id);
+        return new PostDetailResource($post);
+    }
+
+    public function show2($id) 
+    {
+        $post = Post::findOrFail($id);
+        return new PostDetailResource($post);
     }
 }
