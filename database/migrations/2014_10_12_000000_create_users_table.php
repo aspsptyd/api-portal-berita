b0ecd51b82253fae9682eb876325b2164cc894b5
aspsptyd <devadiroot@gmail.com>
2023-07-04 12:45:51 +0700

    Modify table user dan lakukan aksi php artisan migrate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username')->unique();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('firstname', 100);
+            $table->string('lastname', 100)->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
