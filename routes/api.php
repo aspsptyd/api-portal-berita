3c5aea9bf84e3453fcfe0a3c8e4b12142bf430a1
aspsptyd <devadiroot@gmail.com>
2023-07-06 23:07:05 +0700

    Implement Autorization dengan sanctum dan middleware, akses login dan revoke token

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthenticationController;
 use App\Http\Controllers\PostController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/posts', [PostController::class, 'index']);
-Route::get('/post/{id}', [PostController::class, 'show']);
-Route::get('/post2/{id}', [PostController::class, 'show2']);
-Route::get('/post3/{id}', [PostController::class, 'show3']);
+Route::get('/posts', [PostController::class, 'index'])->middleware(['auth:sanctum']);
+Route::get('/post/{id}', [PostController::class, 'show'])->middleware(['auth:sanctum']);
+Route::get('/post2/{id}', [PostController::class, 'show2'])->middleware(['auth:sanctum']);
+Route::get('/post3/{id}', [PostController::class, 'show3'])->middleware(['auth:sanctum']);
+
+Route::post('/login', [AuthenticationController::class, 'login']);
+Route::get('/logout', [AuthenticationController::class, 'logout'])->middleware(['auth:sanctum']);
