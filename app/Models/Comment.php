eda5fbc22e562dadfd853e7dd0a3f736acf44222
aspsptyd <devadiroot@gmail.com>
2023-07-08 23:56:57 +0700

    Tambah comment untuk postingan

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'post_id', 'user_id', 'comments_content'
+    ];
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,8 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/posts', [PostController::class, 'store']);
     Route::patch('/posts/{id}', [PostController::class, 'update'])->middleware('post-creator');
     Route::delete('/posts/{id}', [PostController::class, 'destroy'])->middleware('post-creator');
+
+    Route::post('/comment', [CommentController::class, 'store']);
 });
 
 /**
